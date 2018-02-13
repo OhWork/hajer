18e408106666755881df0e9cdb484a1938da2a6e
Rachaporn <takbasic722@gmail.com>
2018-02-13 21:09:46 +0700

    อัพเดท

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/jquery-ui.css">
+        <link rel="stylesheet" href="CSS/main.css">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    </head>
+    <body>
+        <div class="wrapper">
+        <div class="container">
+            <div class="row">
+				<div class="col-12">
+					<div class="row">
+						<nav class="navbar navbar-expand-lg navbar-light bg-light col-12" style="border-bottom: solid 1px #0097A7">
+							<a class="navbar-brand" href="#">Navbar</a>
+						</nav>
+						<div class="col-12" style="background-color: #0097A7;color:#ffffff;">
+							<div class="row">
+								<div class="col-12">
+									<div class="row">
+										<div class="col-1"></div>
+										<div class="col-10" style="padding-top:8px;">
+											<p style="margin-bottom:8px;">ร้านค้าที่ต้องการค้นหา</p>
+										</div>
+										<div class="col-1"></div>
+									</div>
+								</div>
+								<div class="col-12" style="margin-bottom:8px;">
+									<div class="row">
+										<div class="col-1"></div>
+										<div class="col-4" style="padding-right:0px;"><p style="margin-bottom:0px;padding-top:2px;">เมือง :</p></div>
+										<input class="form-control col-6" style="padding-top:0px;padding-bottom:0px;" type="search" placeholder="Search" aria-label="Search">
+										<div class="col-1"></div>
+									</div>
+								</div>
+								<div class="col-12" style="margin-bottom:8px;">
+									<div class="row">
+										<div class="col-1"></div>
+										<div class="col-4" style="padding-right:0px;"><p style="margin-bottom:0px;padding-top:2px;">เขต/อำเภอ :</p></div>
+										<input class="form-control col-6" style="padding-top:0px;padding-bottom:0px;" type="search" placeholder="Search" aria-label="Search">
+										<div class="col-1"></div>
+									</div>
+								</div>
+								<div class="col-12" style="margin-bottom:8px;">
+									<div class="row">
+										<div class="col-1"></div>
+										<div class="col-4" style="padding-right:0px;"><p style="margin-bottom:0px;padding-top:2px;">แขวง/ตำบล :</p></div>
+										<input class="form-control col-6" style="padding-top:0px;padding-bottom:0px;" type="search" placeholder="Search" aria-label="Search">
+										<div class="col-1"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-12" style="color:#0097A7;background-color: #ffffff;height:250px;border-bottom: solid 1px #0097A7;">
+							<div class="row">
+								<div class="col-12">
+									<div class="row">
+										<div class="col-1"></div>
+										<div class="col-10" style="padding-top:8px;">
+											<p style="margin-bottom:8px;">ส่วนสงเสริมการขาย</p>
+										</div>
+										<div class="col-1"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-12" style="color:#0097A7;background-color: #ffffff;">
+							<div class="row">
+								<div class="col-12">
+									<div class="row">
+										<div class="col-1"></div>
+										<div class="col-10" style="padding-top:8px;">
+											<p style="margin-bottom:8px;">ร้านค้า</p>
+										</div>
+										<div class="col-1"></div>
+									</div>
+								</div>
+								<div class="col-12">
+									<div class="row">
+										<div class="col-2"></div>
+										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home2.png"width="40" height="40"></center></div>
+										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home3.png"width="40" height="40"></center></div>
+										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home4.png"width="40" height="40"></center></div>
+										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home5.png"width="40" height="40"></center></div>
+										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home6.png"width="40" height="40"></center></div>
+										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home7.png"width="40" height="40"></center></div>
+										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home8.png"width="40" height="40"></center></div>
+										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home.png"width="40" height="40"></center></div>
+										<div class="col-2"></div>
+									</div>
+								</div>
+								<div class="col-12">
+									<div class="row">
+										<div class="col-2"></div>
+										<div class="col-8" style="padding-right:0px;padding-left:0px;"><img src="images/maps.png"width="100%"></div>
+										<div class="col-2"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-12" style="background-color: #0097A7;color:#ffffff;height:150px;">
+						</div>
+					</div>
+				</div>
+            </div>
+        </div> <!--end container-->
+        </div> <!--end wrapper-->
+    </body>
+</html>
