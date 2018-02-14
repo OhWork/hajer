92efcbc87d12ad19781cca75fe224338003db759
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-02-14 19:59:41 +0700

    ลองเอาไปดู

--- index.php
+++ index.php
@@ -5,13 +5,40 @@
         <link rel="stylesheet" href="CSS/bootstrap.css">
         <link rel="stylesheet" href="CSS/jquery-ui.css">
         <link rel="stylesheet" href="CSS/main.css">
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+		<style>
+		#map {
+        width: 100%;
+        height: 500px;
+        background-color: grey;
+      }
+    </style>
     </head>
+    <?php
+	    include_once 'inc_js.php';
+    ?>
+ <!-- ส่วน Google Map -->
+    <script>
+	function initMap() {
+		var uluru = {lat: 13.736717, lng: 100.523186};
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 8,
+			center: uluru
+		});
+		var marker = new google.maps.Marker({
+			position: uluru,
+			map: map
+		});
+	}
+	 </script>
+<?php  /*
+	 var uluru คือตัวแปรเก็บค่า ละติจูด ลองติจูด
+	 var map เป็นตัวแปรเรียกมาใช้ โดยดึงมาจากชืื่อ ID คือ Map
+	 zoom คือการ เจาะลงไปในแผนที่
+	เปลี่ยน key ของ api google map ตรง js?key=
+       */
+?>
+ <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDVTkcgy7zjUHk94AZacwogA2I2nRKefAc&callback=initMap"></script>
+ <!-- สิ้นสุด ส่วน googlemap -->
     <body>
         <div class="wrapper">
         <div class="container">
@@ -85,22 +112,68 @@
 								<div class="col-12">
 									<div class="row">
 										<div class="col-2"></div>
-										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home2.png"width="40" height="40"></center></div>
-										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home3.png"width="40" height="40"></center></div>
-										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home4.png"width="40" height="40"></center></div>
-										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home5.png"width="40" height="40"></center></div>
-										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home6.png"width="40" height="40"></center></div>
-										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home7.png"width="40" height="40"></center></div>
-										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home8.png"width="40" height="40"></center></div>
-										<div class="col-1" style="padding-right:0px;padding-left:0px;"><center><img src="images/home.png"width="40" height="40"></center></div>
+										<div class="col-1 iconshop" style="padding-right:0px;padding-left:0px;" value="1">
+											<center>
+												<img src="images/home2.png"width="40" height="40">
+											</center>
+										</div>
+										<div class="col-1 iconshop" style="padding-right:0px;padding-left:0px;" value="2">
+											<center>
+												<img src="images/home3.png"width="40" height="40">
+											</center>
+										</div>
+										<div class="col-1 iconshop" style="padding-right:0px;padding-left:0px;" value="3">
+											<center>
+												<img src="images/home4.png"width="40" height="40">
+											</center>
+										</div>
+										<div class="col-1 iconshop" style="padding-right:0px;padding-left:0px;" value="4">
+											<center>
+												<img src="images/home5.png"width="40" height="40">
+											</center>
+										</div>
+										<div class="col-1 iconshop" style="padding-right:0px;padding-left:0px;" value="5">
+											<center>
+											<img src="images/home6.png"width="40" height="40">
+											</center>
+										</div>
+										<div class="col-1 iconshop" style="padding-right:0px;padding-left:0px;" value="6">
+											<center>
+											<img src="images/home7.png"width="40" height="40">
+											</center>
+										</div>
+										<div class="col-1 iconshop" style="padding-right:0px;padding-left:0px;" value="7">
+											<center>
+											<img src="images/home8.png"width="40" height="40">
+											</center>
+										</div>
+										<div class="col-1 iconshop" style="padding-right:0px;padding-left:0px;" value="8">
+											<center>
+												<img src="images/home.png"width="40" height="40">
+											</center>
+										</div>
 										<div class="col-2"></div>
 									</div>
 								</div>
-								<div class="col-12">
+								<div class="col-12 mt-1">
 									<div class="row">
 										<div class="col-2"></div>
-										<div class="col-8" style="padding-right:0px;padding-left:0px;"><img src="images/maps.png"width="100%"></div>
+										<span class="col-md-1" id="1"></span>
+										<span class="col-md-1" id="2"></span>
+										<span class="col-md-1" id="3"></span>
+										<span class="col-md-1" id="4"></span>
+										<span class="col-md-1" id="5"></span>
+										<span class="col-md-1" id="6"></span>
+										<span class="col-md-1" id="7"></span>
+										<span class="col-md-1" id="8"></span>
 										<div class="col-2"></div>
+									</div>
+								</div>
+								<div class="col-12 mt-5">
+									<div class="row">
+										<div class="col-12" style="padding-right:0px;padding-left:0px;">
+											<div id="map"></div>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -114,3 +187,102 @@
         </div> <!--end wrapper-->
     </body>
 </html>
+ <script>
+	 	$(document).ready(function(){1
+	    var isshowpost = false;
+	$(".iconshop").hover(function(){
+		var key = $(this).attr('value');
+		switch(key){
+			case "1":
+				if(isshowpost){
+					var showname = document.getElementById("1");
+					showname.innerHTML = " ";
+					isshowpost = false;
+				}else{
+					var showname = document.getElementById("1");
+					showname.innerHTML = "<center>1</center>";
+					isshowpost = true;
+				}
+				return isshowpost;
+			case '2' :
+				if(isshowpost){
+					var showname = document.getElementById("2");
+					showname.innerHTML = " ";
+					isshowpost = false;
+				}else{
+					var showname = document.getElementById("2");
+					showname.innerHTML = "<center>2</center>";
+					isshowpost = true;
+				}
+				return isshowpost;
+			case '3' :
+				if(isshowpost){
+					var showname = document.getElementById("3");
+					showname.innerHTML = " ";
+					isshowpost = false;
+				}else{
+					var showname = document.getElementById("3");
+					showname.innerHTML = "<center>3</center>";
+					isshowpost = true;
+				}
+				return isshowpost;
+			case '4' :
+					if(isshowpost){
+					var showname = document.getElementById("4");
+					showname.innerHTML = " ";
+					isshowpost = false;
+				}else{
+					var showname = document.getElementById("4");
+					showname.innerHTML = "<center>4</center>";
+					isshowpost = true;
+				}
+				return isshowpost;
+			case '5' :
+				if(isshowpost){
+					var showname = document.getElementById("5");
+					showname.innerHTML = " ";
+					isshowpost = false;
+				}else{
+					var showname = document.getElementById("5");
+					showname.innerHTML = "<center>5</center>";
+					isshowpost = true;
+				}
+				return isshowpost;
+			case '6' :
+				if(isshowpost){
+					var showname = document.getElementById("6");
+					showname.innerHTML = " ";
+					isshowpost = false;
+				}else{
+					var showname = document.getElementById("6");
+					showname.innerHTML = "<center>6</center>";
+					isshowpost = true;
+				}
+				return isshowpost;
+			case '7' :
+				if(isshowpost){
+					var showname = document.getElementById("7");
+					showname.innerHTML = " ";
+					isshowpost = false;
+				}else{
+					var showname = document.getElementById("7");
+					showname.innerHTML = "<center>7</center>";
+					isshowpost = true;
+				}
+				return isshowpost;
+			case '8' :
+			    if(isshowpost){
+					var showname = document.getElementById("8");
+					showname.innerHTML = " ";
+					isshowpost = false;
+				}else{
+					var showname = document.getElementById("8");
+					showname.innerHTML = "<center>8</center>";
+					isshowpost = true;
+				}
+				return isshowpost;
+		}
+});
+	 });
+
+ </script>
