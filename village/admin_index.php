abd2a0951bbc1347fef9918ad4fd1716cedd0cc0
choatchaw <choatchaw@Choatchaws-MacBook-Pro.local>
2018-03-16 21:24:16 +0700

    สร้าง

---
+++ village/admin_index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="../CSS/bootstrap.css">
+        <link rel="stylesheet" href="../CSS/jquery-ui.css">
+        <link rel="stylesheet" href="../CSS/main.css">
+    </head>
+    <body><?php
+								include_once 'content.php';?>
+        <nav class="navbar navbar-dark bg-dark">
+  <!-- Navbar content -->
+            <a class="navbar-brand" href="#">Adminitrator</a>
+        </nav>
+    </body>
+</html>
