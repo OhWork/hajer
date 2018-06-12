7c4f3011ed7be2a12b9ac513b3dd8c55f4f0b3d5
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-06-12 20:18:53 +0700

    g

--- village/admin_menu.php
+++ village/admin_menu.php
@@ -22,9 +22,8 @@
         <a class="nav-link" href="admin_index.php?url=show_shop.php">shop</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    <li class="nav-item">
+        <a class="nav-link" href="admin_index.php?url=logout.php">logout</a>
+      </li>
   </div>
 </nav>
