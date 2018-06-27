e1b9fc23995220af4a617d6fd4502264511bba19
Firore <takbasic722@gmail.com>
2018-06-27 21:42:45 +0700

    ปิดปุ่มเลย

--- index.php
+++ index.php
@@ -127,11 +127,11 @@
 									  <img class="d-block w-100" src="images/noimage.png" height="199px" alt="Third slide">
 									</div>
 								  </div>
-								  <a class="carousel-control-prev bg-dark" href="#carouselExampleIndicators" role="button" data-slide="prev">
+								  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 									<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 									<span class="sr-only">Previous</span>
 								  </a>
-								  <a class="carousel-control-next bg-dark" href="#carouselExampleIndicators" role="button" data-slide="next">
+								  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
 									<span class="carousel-control-next-icon" aria-hidden="true"></span>
 									<span class="sr-only">Next</span>
 								  </a>
