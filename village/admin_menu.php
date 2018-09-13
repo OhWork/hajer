eeec0d0971064bd2f25d8a4580e79ebdfaa29035
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-09-13 14:24:48 +0700

    CSS บางตัวที่สั่งควบคุมหลังบ้านของ Hajer หายนะ การแสดงผลเลยเพี้ยน

--- village/admin_menu.php
+++ village/admin_menu.php
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Hajersystem</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="admin_index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="admin_index.php?url=show_provinces.php">provinces</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="admin_index.php?url=show_district.php">district</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="admin_index.php?url=show_subdistrict.php">subdistrict</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="admin_index.php?url=show_shop.php">shop</a>
-      </li>
-    </ul>
-    <li class="nav-item">
-        <a class="nav-link" href="logout.php">logout</a>
-      </li>
-  </div>
-</nav>
-=======
 <?php if (!empty($_SESSION['member_username'])):
 ?>
 <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" style="height: 100%;">
@@ -103,5 +72,3 @@
         });
     });
 </script>
-
->>>>>>> 717a7977bb915b375dc10c2c605b4dc8538247ee
