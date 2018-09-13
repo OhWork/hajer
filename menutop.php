5cd7bc571864136fadda1bfe3f004809da204aa2
Firore <takbasic722@gmail.com>
2018-09-13 23:26:27 +0700

    123
    up

--- menutop.php
+++ menutop.php
@@ -1,17 +1,37 @@
 <div class="col-12 boxtop">
 	<div class="row">
 		<div class="col-12">
-			<a href="index.php" style="float:left;">
-				<img src="images/Hajers2.png" width="80" height="50" alt="logo_test">
-			</a>
-			<a class="hvmenu" href="village/login.php">
+			<div class="row">
+				<div class="col-5 logomn">
+					<a class="col-12" href="index.php" style="float:left;">
+						<div class="row">
+							<div class="lg1">Ha</div><div class="lg2">J</div><div class="lg1">er</div>
+						</div>
+					</a>
+				</div>
+			<!--<a class="hvmenu" href="village/login.php">
 				<div class="rounded-circle circlemenutop">
 					<span data-feather="users"></span>
 				</div>
 				<div class="menutoptext" href="village/login.php">
 					Login
 				</div>
-			</a>
+			</a>-->
+				<div class="col-5 pt-2 lg2">
+					<div class="row">
+						<span data-feather="phone"></span>
+						<div class="ml-2 mr-2 lg1">08x-xxx-xxxx</div>
+						<span data-feather="facebook"></span>
+						<span data-feather="twitter"></span>
+						<span data-feather="youtube"></span>
+						<span data-feather="instagram"></span>
+					</div>
+				</div>
+				<div class="col-2 pt-1 pl-5">
+					<a class="btn btn-outline-primary lg2 lgb" href="village/login.php">Register</a>
+					<a class="btn btn-outline-success" href="village/login.php">Login</a>
+				</div>
+			</div>	
 		</div>
 	</div>
 </div>
--- slide.php
+++ slide.php
@@ -1,9 +1,9 @@
-<div id="carouselExampleIndicators" class="carousel slide boxslide" data-ride="carousel">
+<!--<div id="carouselExampleIndicators" class="carousel slide boxslide" data-ride="carousel">
 	<!--<ol class="carousel-indicators">
 		<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
 		<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
 		<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-	</ol>-->
+	</ol>--
 	<div class="carousel-inner picslide">
 		<div class="carousel-item active">
 			<img class="d-block w-100 h-100" src="images/testpic4.jpg" alt="First slide">
@@ -21,11 +21,30 @@
 	<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
 		<span class="sr-only">Next</span>
 	</a>
+</div>-->
+<div class="col-12" style="opacity:0.5;">
+	<div class="row">
+		<img class="d-block w-100 h-100" src="images/mapshow.png">
+	</div>
 </div>
 <div class="col-12 inner_position_top">
 	<div class="row">
-		<div class="col-12 textos">
-			<h4>ค้นหาร้านค้าที่คุณต้องการ</h4>
+		<div class="col-12">
+			<div class="row">
+				<div class="col-4">
+					<center><img src="images/Hajers2.png" width="200" height="150" alt="logo_test"></center>
+				</div>
+				<div class="col-8 pt-5">
+					<div class="row">
+						<div class="col-12 lg2 textos">
+							<h2>FIND THE BEST PLACES TO BE</h2>
+						</div>
+						<div class="col-12 pl-5">
+							<p>All Location - form Shop,hair cut,drugstore and more..</p>
+						</div>
+					</div>
+				</div>
+			</div>	
 		</div>
 		<div class="col-12 mt-3">
 			<div class="row">
@@ -38,7 +57,7 @@
 				</div>
 				</div>
 				<div class="col-1">
-					<div class="btn btn-primary col-12 searchbt"><span data-feather="search"></span></div>
+					<div class="btn btn-primary col-12 searchbt lg2 lg3 lgb"><span data-feather="search"></span></div>
 				</div>
 			</div>
 		</div>
