dab17ff7b366baa8d15dbc1410c1141a5528691a
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-10-02 20:12:55 +0700

    ดันๆ

--- shopinterview.php
+++ shopinterview.php
@@ -1,10 +1,19 @@
+<?php
+	$rs = $db->findAll('shop')->execute();
+
+?>
 <div class="col-12 mt-5">
 	<div class="row">
 		<div class="col-12 svtop">
 			<div class="row">
 				<div class="col-4 lg5">
 					<b><h4>Recommended stores</h4></b>
-					<p class="lg6">Over 1,200 store ads...</p>
+					<p class="lg6">
+					<?php
+						$rslastid = $db->findAllDESC('shop','shop_id')->executeAssoc();
+						echo 'Over ',$rslastid['shop_id'],' store ads...';
+					?>
+					</p>
 				</div>
 				<div class="col-4 svbd"></div>
 				<div class="col-4"></div>
@@ -12,18 +21,21 @@
 		</div>
 		<div class="col-12 mt-4">
 			<div class="row">
+				<?php
+					foreach($rs as $showrs){
+				?>
 				<div class="col-3 svbb pb-3">
 					<div class="col-12 pl-0 pr-0 mt-3">
 						<img src="images/maps.png" width="100%" height="200" style="border-radius:20px;" />
 					</div>
 					<div class="col-12 lg5 svbd mt-3">
-						Shopname
+						<?php echo $showrs['shop_name'];?>
 					</div>
 					<div class="col-12 lg6 mt-2">
 						Last up date: 2 hours ago
 					</div>
 					<div class="col-12 lg5">
-						Price: ???
+						<?php echo $showrs['shop_rateprice']?>
 					</div>
 					<div class="col-12 lg6 mt-3">
 						<div class="row">
@@ -31,7 +43,7 @@
 								<span data-feather="navigation"></span>
 							</div>
 							<div class="col-10 pl-2 pr-0 svadr">
-								99 Sukantaram RD, Dusit
+								<?php echo $showrs['chop_place']?>
 							</div>
 						</div>
 					</div>
@@ -48,272 +60,8 @@
 						<div class="btn lg6 lgb w-100">Show Phone Number</div>
 					</div>
 				</div>
-				<div class="col-3 svbb pb-3">
-					<div class="col-12 pl-0 pr-0 mt-3">
-						<img src="images/maps.png" width="100%" height="200" style="border-radius:20px;" />
-					</div>
-					<div class="col-12 lg5 svbd mt-3">
-						Shopname
-					</div>
-					<div class="col-12 lg6 mt-2">
-						Last up date: 2 hours ago
-					</div>
-					<div class="col-12 lg5">
-						Price: ???
-					</div>
-					<div class="col-12 lg6 mt-3">
-						<div class="row">
-							<div class="col-2 lg2">
-								<span data-feather="navigation"></span>
-							</div>
-							<div class="col-10 pl-2 pr-0 svadr">
-								99 Sukantaram RD, Dusit
-							</div>
-						</div>
-					</div>
-					<div class="col-12">
-						<div class="lg6">Rate:
-						<img src="images/star.png" width="15px" height="15px" style="margin-left:5px;" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						</div>
-					</div>
-					<div class="col-12 mt-2">
-						<div class="btn lg6 lgb w-100">Show Phone Number</div>
-					</div>
-				</div>
-				<div class="col-3 svbb pb-3">
-					<div class="col-12 pl-0 pr-0 mt-3">
-						<img src="images/maps.png" width="100%" height="200" style="border-radius:20px;" />
-					</div>
-					<div class="col-12 lg5 svbd mt-3">
-						Shopname
-					</div>
-					<div class="col-12 lg6 mt-2">
-						Last up date: 2 hours ago
-					</div>
-					<div class="col-12 lg5">
-						Price: ???
-					</div>
-					<div class="col-12 lg6 mt-3">
-						<div class="row">
-							<div class="col-2 lg2">
-								<span data-feather="navigation"></span>
-							</div>
-							<div class="col-10 pl-2 pr-0 svadr">
-								99 Sukantaram RD, Dusit
-							</div>
-						</div>
-					</div>
-					<div class="col-12">
-						<div class="lg6">Rate:
-						<img src="images/star.png" width="15px" height="15px" style="margin-left:5px;" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						</div>
-					</div>
-					<div class="col-12 mt-2">
-						<div class="btn lg6 lgb w-100">Show Phone Number</div>
-					</div>
-				</div>
-				<div class="col-3 svbb pb-3">
-					<div class="col-12 pl-0 pr-0 mt-3">
-						<img src="images/maps.png" width="100%" height="200" style="border-radius:20px;" />
-					</div>
-					<div class="col-12 lg5 svbd mt-3">
-						Shopname
-					</div>
-					<div class="col-12 lg6 mt-2">
-						Last up date: 2 hours ago
-					</div>
-					<div class="col-12 lg5">
-						Price: ???
-					</div>
-					<div class="col-12 lg6 mt-3">
-						<div class="row">
-							<div class="col-2 lg2">
-								<span data-feather="navigation"></span>
-							</div>
-							<div class="col-10 pl-2 pr-0 svadr">
-								99 Sukantaram RD, Dusit
-							</div>
-						</div>
-					</div>
-					<div class="col-12">
-						<div class="lg6">Rate:
-						<img src="images/star.png" width="15px" height="15px" style="margin-left:5px;" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						</div>
-					</div>
-					<div class="col-12 mt-2">
-						<div class="btn lg6 lgb w-100">Show Phone Number</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-12 mt-4">
-			<div class="row">
-				<div class="col-3 svbb pb-3">
-					<div class="col-12 pl-0 pr-0 mt-3">
-						<img src="images/maps.png" width="100%" height="200" style="border-radius:20px;" />
-					</div>
-					<div class="col-12 lg5 svbd mt-3">
-						Shopname
-					</div>
-					<div class="col-12 lg6 mt-2">
-						Last up date: 2 hours ago
-					</div>
-					<div class="col-12 lg5">
-						Price: ???
-					</div>
-					<div class="col-12 lg6 mt-3">
-						<div class="row">
-							<div class="col-2 lg2">
-								<span data-feather="navigation"></span>
-							</div>
-							<div class="col-10 pl-2 pr-0 svadr">
-								99 Sukantaram RD, Dusit
-							</div>
-						</div>
-					</div>
-					<div class="col-12">
-						<div class="lg6">Rate:
-						<img src="images/star.png" width="15px" height="15px" style="margin-left:5px;" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						</div>
-					</div>
-					<div class="col-12 mt-2">
-						<div class="btn lg6 lgb w-100">Show Phone Number</div>
-					</div>
-				</div>
-				<div class="col-3 svbb pb-3">
-					<div class="col-12 pl-0 pr-0 mt-3">
-						<img src="images/maps.png" width="100%" height="200" style="border-radius:20px;" />
-					</div>
-					<div class="col-12 lg5 svbd mt-3">
-						Shopname
-					</div>
-					<div class="col-12 lg6 mt-2">
-						Last up date: 2 hours ago
-					</div>
-					<div class="col-12 lg5">
-						Price: ???
-					</div>
-					<div class="col-12 lg6 mt-3">
-						<div class="row">
-							<div class="col-2 lg2">
-								<span data-feather="navigation"></span>
-							</div>
-							<div class="col-10 pl-2 pr-0 svadr">
-								99 Sukantaram RD, Dusit
-							</div>
-						</div>
-					</div>
-					<div class="col-12">
-						<div class="lg6">Rate:
-						<img src="images/star.png" width="15px" height="15px" style="margin-left:5px;" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						</div>
-					</div>
-					<div class="col-12 mt-2">
-						<div class="btn lg6 lgb w-100">Show Phone Number</div>
-					</div>
-				</div>
-				<div class="col-3 svbb pb-3">
-					<div class="col-12 pl-0 pr-0 mt-3">
-						<img src="images/maps.png" width="100%" height="200" style="border-radius:20px;" />
-					</div>
-					<div class="col-12 lg5 svbd mt-3">
-						Shopname
-					</div>
-					<div class="col-12 lg6 mt-2">
-						Last up date: 2 hours ago
-					</div>
-					<div class="col-12 lg5">
-						Price: ???
-					</div>
-					<div class="col-12 lg6 mt-3">
-						<div class="row">
-							<div class="col-2 lg2">
-								<span data-feather="navigation"></span>
-							</div>
-							<div class="col-10 pl-2 pr-0 svadr">
-								99 Sukantaram RD, Dusit
-							</div>
-						</div>
-					</div>
-					<div class="col-12">
-						<div class="lg6">Rate:
-						<img src="images/star.png" width="15px" height="15px" style="margin-left:5px;" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						</div>
-					</div>
-					<div class="col-12 mt-2">
-						<div class="btn lg6 lgb w-100">Show Phone Number</div>
-					</div>
-				</div>
-				<div class="col-3 svbb pb-3">
-					<div class="col-12 pl-0 pr-0 mt-3">
-						<img src="images/maps.png" width="100%" height="200" style="border-radius:20px;" />
-					</div>
-					<div class="col-12 lg5 svbd mt-3">
-						Shopname
-					</div>
-					<div class="col-12 lg6 mt-2">
-						Last up date: 2 hours ago
-					</div>
-					<div class="col-12 lg5">
-						Price: ???
-					</div>
-					<div class="col-12 lg6 mt-3">
-						<div class="row">
-							<div class="col-2 lg2">
-								<span data-feather="navigation"></span>
-							</div>
-							<div class="col-10 pl-2 pr-0 svadr">
-								99 Sukantaram RD, Dusit
-							</div>
-						</div>
-					</div>
-					<div class="col-12">
-						<div class="lg6">Rate:
-						<img src="images/star.png" width="15px" height="15px" style="margin-left:5px;" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						<img src="images/star.png" width="15px" height="15px" />
-						</div>
-					</div>
-					<div class="col-12 mt-2">
-						<div class="btn lg6 lgb w-100">Show Phone Number</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-12 mt-3">
-			<div class="row">
-				<div class="col-5"></div>
-				<div class="col-2">
-					<input class="btn lg2 bgw lgb w-100" type="button" value="View all" />
-				</div>
-				<div class="col-5"></div>
-			</div>
-		</div>
+				<?php
+					}
+				?>
 	</div>
 </div>
