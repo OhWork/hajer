910ae4286a39bbbe5dcf5ca5053d1ff8148acbe4
Firore <takbasic722@gmail.com>
2018-10-31 00:05:09 +0700

    up

--- footer.php
+++ footer.php
@@ -1,5 +1,74 @@
-<div class="col-12 boxfooter">
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 boxfooter">
 	<div class="row">
-		<div class="col-12 lg3" style="height:150px;"></div>
+		<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg3 bb2">
+			<div class="row">
+				<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mt-3">
+					<div class="row">
+						<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>About Hajer</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>- menu</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>- menu</p>
+							</div>
+						</div>
+						<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>Site maps</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>Register</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>Login</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mt-3">
+					<div class="row">
+						<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>Hajer contact</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>Hajer.co.ltd</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>789 Main rd., Dusit, Dusit, Bangkok 10300</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>Tel : 0xx-xxx-xxx</p>
+								<p>Tel : 0xx-xxx-xxx</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>Line ID : ZINGZING</p>
+							</div>
+						</div>
+						<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>Site maps</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>Register</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+								<p>Login</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg3">
+			<div class="row">
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7 mt-3">
+					<p>copy right by hajer.co.ltd., companygroup with friends</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
