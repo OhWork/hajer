6039722efc261bbf68cfa8eef941ee19c3b06ec2
Firore <takbasic722@gmail.com>
2018-11-20 22:33:58 +0700

    up

--- menutop.php
+++ menutop.php
@@ -1,16 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-light">
-	<a class="navbar-brand logomn" href="#">
+	<a class="navbar-brand logomn mgt" href="#">
 		<img height="50px;" src="images/Hajers2.png">
 	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
-	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="navbar-nav mgt">
-		</ul>
-	</div>
-	<div class="dropdown lg2">
-		<div style="height:34px;" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	<div class="collapse navbar-collapse dropdown lg2">
+		<div class="navbar-nav" style="height:34px;" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 			<div style="float:left;margin-top:4px;">
 				เข้าสู่ระบบ
 			</div>
