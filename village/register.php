bbe7f52c7bb84dc1f8086d144ef754899d27fe1d
Firore <takbasic722@gmail.com>
2018-11-28 22:01:50 +0700

    ีup

--- village/register.php
+++ village/register.php
@@ -1,1 +1,17 @@
-test
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12" align="center">
+	<p>สมัครสมาชิก หาเจอ</p>
+</div>
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12" align="center">
+	<div class="row">
+		<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+			<div class="row">
+				<img >
+			</div>
+		</div>
+		<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+			<div class="row">
+				<input class="col-xl-12 col-lg-12 col-md-12 col-sm-12 w-100" name="อีเมล" type="text">
+			</div>
+		</div>
+	</div>
+</div>
