3433adc158bc3f1829012d1781a71ed2e1e1f564
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-12-14 20:20:59 +0700

    แชร์ต้องรอ เป็น โดเมนจริงๆ

--- index.php
+++ index.php
@@ -16,6 +16,14 @@
         <link rel="stylesheet" href="CSS/select2.min.css">
 		<link rel="stylesheet" href="CSS/main.css">
 	</head>
+	<script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
     <body>
 		<?php include "menutop.php" ?>
 		<?php include "content.php" ?>
--- shopopen.php
+++ shopopen.php
@@ -83,7 +83,12 @@
 																<div class="row">
 <!-- 																	<button type="button" class="btn btn-primary col-xl-4 col-lg-4 col-md-4 col-sm-4"></button> -->
 																	<button type="button" class="btn btn-primary col-xl-4 col-lg-4 col-md-4 col-sm-4">เช็คอิน</button>
-																	<button type="button" class="btn btn-primary col-xl-4 col-lg-4 col-md-4 col-sm-4">แชร์</button>
+																	<!-- แก้๊URL ใน Data-href																 -->
+																	<div class="fb-share-button"
+																    data-href="URL"
+																    data-layout="button_count"
+																	data-size="large">
+																  </div>
 																</div>
 															</div>
 														</div>
