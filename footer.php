6137c77a1ade0ef178bdcae30e841aa3e8d37639
Firore <takbasic722@gmail.com>
2018-12-17 23:13:28 +0700

    ีup

--- footer.php
+++ footer.php
@@ -2,62 +2,69 @@
 	<div class="row">
 		<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg3 bb2">
 			<div class="row">
-				<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mt-3">
+				<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 mt-3">
 					<div class="row">
-						<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>About Hajer</p>
+						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3"></div>
+						<div class="col-xl-5 col-lg-5 col-md-5 col-sm-5">
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lgw">
+								<p>เมนู</p>
 							</div>
 							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>- menu</p>
+								<p>- แผนผั่งเว็บไซต์</p>
 							</div>
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>- menu</p>
+							<a class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7" href="index.php?url=aboutus.php">
+								- เกี่ยวกับหาเจอ
+							</a>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7 mt-3">
+								<p>- เข้าสู่ระบบ</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7 mt-3">
+								<p>- เรทร้านคืออะไร</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7 mt-3">
+								<p>- ข่าวประชาสัมพันธ์</p>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7 mt-3">
+								<p>- คำถามที่พบบ่อย</p>
 							</div>
 						</div>
-						<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>Site maps</p>
-							</div>
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>Register</p>
-							</div>
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>Login</p>
-							</div>
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4"></div>
+					</div>	
+				</div>
+				<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 mt-3">
+					<div class="row">
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lgw">
+							<p>สำหรับร้านหรือธุรกิจ</p>
+						</div>
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+							<p>- การประชาสัมพันธ์ร้านกับหาเจอ</p>
+						</div>
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+							<p>- ระบบจัดการร้านค้า</p>
+						</div>
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+							<p>- การเป็นสมาชิกร้านค้ากับหาเจอ</p>
+						</div>
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+							<p>- บทความเกี่ยวกับการทำธุรกิจ</p>
 						</div>
 					</div>
 				</div>
-				<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mt-3">
+				<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 mt-3">
 					<div class="row">
-						<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>Hajer contact</p>
-							</div>
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>Hajer.co.ltd</p>
-							</div>
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>789 Main rd., Dusit, Dusit, Bangkok 10300</p>
-							</div>
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>Tel : 0xx-xxx-xxx</p>
-								<p>Tel : 0xx-xxx-xxx</p>
-							</div>
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>Line ID : ZINGZING</p>
-							</div>
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lgw">
+							<p>ติดต่อหาเจอ</p>
 						</div>
-						<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>Site maps</p>
-							</div>
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>Register</p>
-							</div>
-							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-								<p>Login</p>
-							</div>
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+							<p>- สถานที่ตั้งหาเจอ</p>
+						</div>
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+							<p>- ติดตามหาเจอ</p>
+						</div>
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+							<span class="lg7 lgbw pt-1 pb-1 ml-3" data-feather="facebook"></span>
+							<span class="lg7 lgbw pt-1 pb-1 ml-1" data-feather="youtube"></span>
+							<span class="lg7 lgbw pt-1 pb-1 ml-1" data-feather="twitter"></span>
 						</div>
 					</div>
 				</div>
