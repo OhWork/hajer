822914c8dfaea7adab376ebb35a6fbb6fa282627
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-12-25 21:20:42 +0700

    ก็ทำงานนะ

--- village/admin_menu_manshop.php
+++ village/admin_menu_manshop.php
@@ -1,13 +1,13 @@
-                <a class="nav-link collapsed py-1" href="#manshop" data-toggle="collapse" data-target="#manshop">
+                <a class="nav-link collapsed py-1 lgw" href="#manshop" data-toggle="collapse" data-target="#manshop">
                   <span data-feather="shopping-cart"></span>
                   ระบบจัดการร้านค้า
                 </a>
                    <!-- sub menu -->
-                <ul class="sub-menu collapse" id="manshop">
-	                <a class="nav-link dropuser" href="admin_index.php?url=show_shop.php">
+                <ul class="sub-menu collapse " id="manshop">
+	                <a class="nav-link dropuser lgw" href="admin_index.php?url=show_shop.php">
 	                   <span data-feather="chevron-right"></span>รายการแสดงร้านค้า
 	                </a>
-	                <a class="nav-link dropuser" href="admin_index.php?url=show_typeshop.php">
+	                <a class="nav-link dropuser lgw" href="admin_index.php?url=show_typeshop.php">
 	                   <span data-feather="chevron-right"></span>รายการแสดงประเภทร้านค้า
 	                </a>
                 </ul>
--- village/admin_menu_manweb.php
+++ village/admin_menu_manweb.php
@@ -1,18 +1,18 @@
-                <a class="nav-link collapsed py-1" href="#manweb" data-toggle="collapse" data-target="#manweb">
+                <a class="nav-link collapsed py-1 lgw" href="#manweb" data-toggle="collapse" data-target="#manweb">
                   <span data-feather="chrome"></span>
                   ระบบจัดการเว็บไซต์
                 </a>
                    <!-- sub menu -->
                 <ul class="sub-menu collapse" id="manweb">
-	                <a class="nav-link dropuser" href="admin_index.php?url=show_subdistrict.php">
+	                <a class="nav-link dropuser lgw" href="admin_index.php?url=show_subdistrict.php">
 	                   <span data-feather="chevron-right"></span>รายการตำบล
 	                </a>
-	                <a class="nav-link dropuser" href="admin_index.php?url=show_district.php">
+	                <a class="nav-link dropuser lgw" href="admin_index.php?url=show_district.php">
 	                   <span data-feather="chevron-right"></span>รายการอำเภอ
 	                </a>
-	                <a class="nav-link dropuser" href="admin_index.php?url=show_provinces.php">
+	                <a class="nav-link dropuser lgw" href="admin_index.php?url=show_provinces.php">
 	                   <span data-feather="chevron-right"></span>รายการจังหวัด
-	                <a class="nav-link dropuser" href="admin_index.php?url=log.php">
+	                <a class="nav-link dropuser lgw" href="admin_index.php?url=log.php">
 	                   <span data-feather="chevron-right"></span>Log-การใช้งาน
 	                </a>
                 </ul>
