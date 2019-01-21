eca3e925f44056e1d23bdc4329a33a076357706e
Firore <takbasic722@gmail.com>
2019-01-21 23:23:38 +0700

    up

--- footer.php
+++ footer.php
@@ -10,17 +10,11 @@
 								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lgw pl-0">
 									<p><b>เมนู</b></p>
 								</div>
-								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-									<p>แผนผั่งเว็บไซต์</p>
-								</div>
 								<a class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7 mb-3" href="index.php?url=aboutus.php">
 									เกี่ยวกับหาเจอ
 								</a>
 								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-									<p>เข้าสู่ระบบ</p>
-								</div>
-								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-									<p>เรทร้านคืออะไร</p>
+									<p>โปรโมชั่น</p>
 								</div>
 								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
 									<p>ข่าวประชาสัมพันธ์</p>
@@ -42,16 +36,10 @@
 									<p><b>สำหรับร้านหรือธุรกิจ</b></p>
 								</div>
 								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-									<p>การประชาสัมพันธ์ร้านกับหาเจอ</p>
+									<p>ร่วมเป็นส่วนหนึ่งกับหาเจอ</p>
 								</div>
 								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-									<p>ระบบจัดการร้านค้า</p>
-								</div>
-								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-									<p>การเป็นสมาชิกร้านค้ากับหาเจอ</p>
-								</div>
-								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-									<p>บทความเกี่ยวกับการทำธุรกิจ</p>
+									<p>โปรโมทร้านกับหาเจอ</p>
 								</div>
 							</div>
 						</div>
@@ -60,17 +48,17 @@
 				</div>
 				<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 mt-3">
 					<div class="row">
-						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3"></div>
-						<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+						<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1"></div>
+						<div class="col-xl-10 col-lg-10 col-md-10 col-sm-10">
 							<div class="row">
 								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lgw pl-0">
+									<p><b>สำหรับสมาชิก</b></p>
+								</div>
+								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
+									<p>สมัครสามชิกหาเจอ</p>
+								</div>
+								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lgw pl-0">
 									<p><b>ติดต่อหาเจอ</b></p>
-								</div>
-								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-									<p>สถานที่ตั้งหาเจอ</p>
-								</div>
-								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 lg7">
-									<p>ติดต่อหาเจอ</p>
 								</div>
 								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
 									<span class="lg7 lgbw pt-1 pb-1 ml-3" data-feather="facebook"></span>
@@ -79,7 +67,7 @@
 								</div>
 							</div>
 						</div>
-						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3"></div>
+						<div class="col-xl-1 col-lg-1 col-md-1 col-sm-1"></div>
 					</div>
 				</div>
 			</div>
