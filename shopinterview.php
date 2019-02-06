35bd96c6ed49d0d75e3c9ffc7ce8e712497ac100
Firore <takbasic722@gmail.com>
2019-02-06 23:23:53 +0700

    up

--- shopinterview.php
+++ shopinterview.php
@@ -28,52 +28,62 @@
 						$rsselect= $db->specifytable("SUM(review_rate),COUNT(member_member_id)","review","review_shop_id = $shop_id")->executeAssoc();
 				?>
 				<div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 svbb pb-3">
-					<div class="col-12 pl-0 pr-0 mt-3">
-						<?php
-							if($showrs['shop_pic'] != ''){
-						?>
-								<img src="images/shop/<?php echo $showrs['shop_pic'];?>" width="100%" height="200" style="border-radius:20px;" />
-						<?php
-							}else{
-								?>
-								<img src="images/noimage.png" width="100%" height="200" style="border-radius:20px;" />
-						<?php
-							}
-						?>
-					</div>
-					<div class="col-12 lg5 svbd mt-3">
-						<p><?php echo $showrs['shop_name'];?>
-						<img src="<?php echo $showrs['typeshop_pathpic']; ?>" width="20px" height="20px" style="float: right;"/>
-						</p>
-					</div>
-					<div class="col-12 lg5">
-						ช่วงราคา : <?php echo $showrs['shop_ratepricemin'],' - ',$showrs['shop_ratepricemax'],' บาท';?>
-					</div>
-					<div class="col-12 lg6 mt-3">
-						<div class="row">
-							<div class="col-4">นำทาง : </div>
-							<div class="col-1 lg2">
-								<a href="https://www.google.com/maps/dir/?api=1&destination=<?php echo $showrs['shop_locationx'],',',$showrs['shop_locationy'];?>"><span data-feather="navigation"></span></a>
-							</div>
-							<div class="col-6 pl-2 pr-0 svadr">
-							</div>
-						</div>
-					</div>
-					<div class="col-12">
-						<div class="row">
-							<div class="col-md-2 lg6">Rate:</div>
+					<div class="row">
+						<div class="col-md-4 col-sm-3 col-3"></div>
+						<div class="col-xl-12 col-lg-12 col-md-4 col-sm-6 col-6 mt-3">
 							<?php
-								  $rate = $rsselect['SUM(review_rate)'];
-								  $memberrate = $rsselect['COUNT(member_member_id)'];
-								  $avgrate = ($rate/$memberrate); ?>
-							<div class="col-md-10 lg6" id="rate<?php echo $i;?>" class="starrate">
-								<input type="hidden" id="shop_id" value="<?php echo $showrs['shop_id'];?>">
-								<input type="hidden" id="avg_rating" value="<?php echo $avgrate;?>">
+								if($showrs['shop_pic'] != ''){
+							?>
+									<img src="images/shop/<?php echo $showrs['shop_pic'];?>" width="100%" height="200" style="border-radius:20px;" />
+							<?php
+								}else{
+									?>
+									<img src="images/noimage.png" width="100%" height="200" style="border-radius:20px;" />
+							<?php
+								}
+							?>
+						</div>
+						<div class="col-12 lg5 mt-2">
+							<p class="svbd"><?php echo $showrs['shop_name'];?>
+							<img src="<?php echo $showrs['typeshop_pathpic']; ?>" width="20px" height="20px" style="float: right;"/>
+							</p>
+						</div>
+						<div class="col-12 lg5">
+							ช่วงราคา : <?php echo $showrs['shop_ratepricemin'],' - ',$showrs['shop_ratepricemax'],' บาท';?>
+						</div>
+						<div class="col-12 lg6 mt-2">
+							<div class="row">
+								<div class="col-4">นำทาง : </div>
+								<div class="col-1 lg2">
+									<a href="https://www.google.com/maps/dir/?api=1&destination=<?php echo $showrs['shop_locationx'],',',$showrs['shop_locationy'];?>"><span data-feather="navigation"></span></a>
+								</div>
+								<div class="col-6 pl-2 pr-0 svadr">
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="col-12 mt-2">
-						<a class="btn lgb w-100" href="index.php?url=shopopen.php&id=<?php echo $showrs['shop_id'];?>">ดูรายละเอียดเพิ่มเติม</a>
+						<div class="col-12 mt-2">
+							<div class="row">
+								<div class="col-md-2 lg6">Rate:</div>
+								<?php
+									  $rate = $rsselect['SUM(review_rate)'];
+									  $memberrate = $rsselect['COUNT(member_member_id)'];
+									  $avgrate = ($rate/$memberrate); ?>
+								<div class="col-md-10 lg6" id="rate<?php echo $i;?>" class="starrate">
+									<input type="hidden" id="shop_id" value="<?php echo $showrs['shop_id'];?>">
+									<input type="hidden" id="avg_rating" value="<?php echo $avgrate;?>">
+								</div>
+							</div>
+						</div>
+						<div class="col-12 mt-2">
+							<div class="row">
+								<div class="col-xl-12 col-lg-12 col-md-4 col-sm-3 col-3"></div>
+								<div class="col-xl-12 col-lg-12 col-md-4 col-sm-6 col-6">
+									<a class="btn lgb w-100" href="index.php?url=shopopen.php&id=<?php echo $showrs['shop_id'];?>">ดูรายละเอียดเพิ่มเติม</a>
+								</div>
+								<div class="col-sm-3 col-3"></div>
+							</div>
+						</div>
+						<div class="col-xl-12 col-lg-12 col-md-4 col-sm-3 col-3"></div>
 					</div>
 				</div>
 				<?php
@@ -81,10 +91,10 @@
 					}
 				?>
 
-	</div>
+			</div>
 	<input type="hidden" id="num_shop" value="<?php echo $rslastid['shop_id'];?>">
 	<input type="hidden" id="member_id" value="<?php echo $_SESSION['member_id'];?>">
-</div>
+		</div>
 <script>
 $(document).ready(function(){
 	var num_shop = $('#num_shop').val();
